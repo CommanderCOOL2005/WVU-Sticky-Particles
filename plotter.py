272e6e04ef819b22d5bce6d72b5c462855ab0a47
Jack Curtis <jackcurt.com@gmail.com>
2025-07-01 11:15:54 -0400

    attempted to fix bug and failed

--- plotter.py
+++ plotter.py
@@ -37,20 +37,29 @@ def plot_ghost_trajectories(system: ParticleSystem, total_time: float, steps: in
 def _plot_ghost_state(system: ParticleSystem, total_time: float, steps: int, fig, ax):
     plot_ghost_trajectories(system, total_time, steps, 0, fig, ax)
     
+#BUG: steps can go into the negatives causing the graphing to break
 def _plot_real_state(system: ParticleSystem, total_time: float, steps: int, fig = None, ax = None):
     system_copy = copy.deepcopy(system)
     elapsed_time = 0
+    temp_total_steps = 0
     while True:
         next_collision = system_copy.get_next_collision()
         delta_time = min(next_collision.time, total_time - elapsed_time)
-        substeps = max(2,int(steps*delta_time/(total_time-elapsed_time)))
+        substeps = None
+        if total_time == elapsed_time:
+            substeps = steps
+        else:
+            substeps = max(2,int(steps*delta_time/(total_time-elapsed_time)))
         plot_ghost_trajectories(system_copy, delta_time, substeps, elapsed_time, fig, ax)
         system_copy.advance(delta_time, next_collision)
         elapsed_time += delta_time
+        temp_total_steps += substeps
+        print(f"{substeps:5}|{temp_total_steps:5}|{steps:5}|{delta_time:3f}| {elapsed_time:3f}")
         if (total_time-elapsed_time) != 0:
             steps -= int(steps*delta_time/(total_time-elapsed_time))
         if(elapsed_time >= total_time):
              break
+    print()
 
 def plot(system: ParticleSystem, total_time: float, steps: int, plot_real_state: bool= True, plot_ghost_state: bool = True):
     axNum = int(plot_ghost_state) + int(plot_real_state)
@@ -58,7 +67,6 @@ def plot(system: ParticleSystem, total_time: float, steps: int, plot_real_state:
     if axNum == 1: #this is the dumbest thing i have ever had to write why is matplotlib the stupidest library on earth why would it do something like this
         axes = [axes]
     xbounds = _get_x_bounds(system)
-    print(xbounds)
     for ax in axes:
         ax.set(xlim = xbounds,
                ybound = (0, total_time)) 
