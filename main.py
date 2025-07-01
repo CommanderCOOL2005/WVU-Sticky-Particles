cb0c3d098360380e203180cdc329be0a3553594d
Jack Curtis <jackcurt.com@gmail.com>
2025-07-01 14:31:32 -0400

    removed message because thats not what the program does anymore

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# Given an array of particles, (m_i, y_i),
-# where m is the mass and the y is the position,
-# find the perfect solution for the repulsive pressureless Euler-Poisson system.
-# The perfect solution is defined as the solution where all collions are glancing
-# (both particles have the same velocity) and an equllibrium is reached.
-# Additionally, total momentum of the system is 0.
-# Conservation of momentum holds
-
 from random import *
 
 from particle import *
