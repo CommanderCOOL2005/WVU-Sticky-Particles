97effb4f13269bbe9fbb4404d38ada04e193466c
Jack Curtis <jackcurt.com@gmail.com>
2025-07-25 15:19:19 -0400

    added function to help with mixing colors

--- helper.py
+++ helper.py
@@ -8,3 +8,7 @@ def sgn(x):
 
 def map_range(x, input_start, input_end, output_start, output_end):
     return  (x - input_start) / (input_end - input_start) * (output_end - output_start) + output_start
+
+
+def mix_colors(a,b,amount):
+    return [(1-amount)*a[i] + amount*b[i] for i in range(len(a))]
